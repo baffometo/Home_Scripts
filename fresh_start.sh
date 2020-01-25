7bf7c29b6cffc2a3564e5696c0c09f2586b5aa17
baffometo <19743348+baffometo@users.noreply.github.com>
2020-01-25 09:57:43 -0500

    Update fresh_start.sh

--- fresh_start.sh
+++ fresh_start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -   
 #title          :fresh_Start
 #description    :This is for John Fresh Start
-#author         :Baffo  
+#author         :Baffo
 #date           :20191026
-#version        :1.0    
+#version        :1.0
 #usage          :
-#notes          :       
+#notes          :
 #bash_version   :4.4.20(1)-release
 #============================================================================
 
@@ -14,9 +14,9 @@ figlet Baffo
 echo "Hello friend, another fresh install?"
 echo "dont you get bored reinstalling OS?"
 echo
-echo "I see that you installed" 
+echo "I see that you installed"
 lsb_release -d
-echo 
+echo
 echo
 echo "OK Which one do you need?"
 echo
